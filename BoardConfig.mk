29792ca818bb80531a597dafc70b84ae6994bab8
thp1997 <tirthpatel49@gmail.com>
2017-12-29 16:35:32 +0000

    X00P: Compile HWUI for Better Performance
    Change-Id: I8c42229aea1b5ad789916756132fab528b317020
    Signed-off-by: Albert I <krascgq@outlook.co.id>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -121,6 +121,9 @@ TARGET_USES_ION := true
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
 DEVICE_MATRIX_FILE := $(DEVICE_PATH)/compatibility_matrix.xml
 
+# HWUI
+HWUI_COMPILE_FOR_PERF := true
+
 # Kernel
 BOARD_KERNEL_BASE := 0x80000000
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 androidboot.bootdevice=7824900.sdhci earlycon=msm_hsl_uart,0x78B0000
