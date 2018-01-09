7edea105e3c6f23d75c01e71df8fa949a414158b
Suren Baghdasaryan <surenb@google.com>
2018-01-09 15:47:40 -0800

    X00P: Set lmkd configuration properties
    Bug: 63631020
    
    Change-Id: I16a183e8e4625e8b98cefdc9c8a7b650556a2b55
    Signed-off-by: Suren Baghdasaryan <surenb@google.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -173,6 +173,7 @@ TARGET_USES_INTERACTION_BOOST := true
 TARGET_HAS_NO_POWER_STATS := true
 
 # Properties
+TARGET_PRODUCT_PROP += $(DEVICE_PATH)/product.prop
 TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
 
 # Qualcomm support
