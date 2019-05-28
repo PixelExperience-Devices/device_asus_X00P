0778c289a60116eb42df38955eef5670e691be82
Felix <google@ix5.org>
2019-05-28 14:05:51 +0200

    X00P: sepolicy: Use BOARD_VENDOR_SEPOLICY_DIRS
    BOARD_SEPOLICY_DIRS is deprecated.
    
    Change-Id: I046282b2a2e8c541726fb29cb0044503322d4be9

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -190,7 +190,7 @@ VENDOR_SECURITY_PATCH := 2020-03-05
 
 # SELinux
 include device/qcom/sepolicy-legacy-um/sepolicy.mk
-BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
+BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 
 # Treble
 BOARD_PROPERTY_OVERRIDES_SPLIT_ENABLED := true
