e1d1b5f0a63863089033adb76365c3e413a6cce2
Dhina17 <dhinalogu@gmail.com>
2020-01-29 17:38:52 +0100

    X00P: Move ConfigStore flags to system properties
    * In Android 10, ConfigStore HAL is replaced by System properties
      But Still ConfigStore is alive to support legacy vendor partitions
    
    Signed-off-by: Dhina17 <dhinalogu@gmail.com>
    Signed-off-by: iamsaalim <saalimquadri1@gmail.com>
    Change-Id: Ibebaf815874130381f3621c313c44584ef21297b

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -179,6 +179,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
     debug.sf.early_gl_phase_offset_ns=3000000 \
     debug.sf.early_gl_app_phase_offset_ns=15000000
 
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+    ro.surface_flinger.force_hwc_copy_for_virtual_displays = true \
+    ro.surface_flinger.max_virtual_display_dimension = 4096
+
 # Time Services
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.vendor.delta_time.enable=true \
