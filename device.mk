116ae6d4ea635f6ce3380e96dda8f0dced891884
MD Raza <raza231198@gmail.com>
2020-03-15 16:30:53 +0530

    X00P: Build More Camera Packages
    Signed-off-by: MD Raza <raza231198@gmail.com>
    Change-Id: Ia1054ed6c40cacbc51aa919302764cf63559df16

--- device.mk
+++ device.mk
@@ -108,8 +108,15 @@ PRODUCT_PACKAGES += \
     Snap
 
 PRODUCT_PACKAGES += \
+    camera.device@1.0-impl \
+    camera.device@3.2-impl \
     android.hardware.camera.provider@2.4-impl \
-    android.hardware.camera.provider@2.4-service
+    android.hardware.camera.provider@2.4-service \
+    vendor.qti.hardware.camera.device@1.0 \
+    vendor.qti.hardware.camera.device@1.0_vendor \
+    libmmjpeg_interface \
+    libmm-qcamera \
+    android.frameworks.displayservice@1.0
 
 # CNE
 PRODUCT_PACKAGES += \
