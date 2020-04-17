4cd58c9477d4586209e0a321a8813167ca55486b
iamsaalim <saalimquadri1@gmail.com>
2020-04-17 11:59:57 +0000

    X00P: update vendor security patch
    Signed-off-by: iamsaalim <saalimquadri1@gmail.com>
    Change-Id: I80a90cc97114b87486ae88d53cfeb70975e811b3

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -180,7 +180,7 @@ TARGET_RIL_VARIANT := caf
 TARGET_USES_OLD_MNC_FORMAT := true
 
 # Security patch level
-VENDOR_SECURITY_PATCH := 2019-07-05
+VENDOR_SECURITY_PATCH := 2020-03-05
 
 # SELinux
 include device/qcom/sepolicy-legacy-um/sepolicy.mk
