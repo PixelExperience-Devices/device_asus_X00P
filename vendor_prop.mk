8be98426a416aef85589ff587089c4d916557a66
Redick Lin <redicklin@google.com>
2020-04-20 18:28:53 +0800

    X00P: BOARD_CHARGER_ENABLE_SUSPEND becomes ro.charger.enable_suspend
    Define ro.charger.enable_suspend to true as charger no
    longer uses BOARD_CHARGER_ENABLE_SUSPEND.
    
    Bug: 153937817
    Change-Id: Ic76bf3a66c5e21c74e49607f54d6c0358c31bbac
    Signed-off-by: Redick Lin <redicklin@google.com>

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -71,6 +71,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
     vidc.enc.dcvs.extra-buff-count=2 \
     persist.camera.CDS=off
 
+# Charger
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.charger.enable_suspend=true
+
 # CNE
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.vendor.cne.feature=1
