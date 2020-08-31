3f8e9eaed597d9a833a49594af008de54255d41c
iamsaalim <saalimquadri1@gmail.com>
2020-08-31 12:26:13 +0530

    X00P: Inherit aosp_X00P via AOSP way * Bringup pixelexperience * Add a dependency file
    Change-Id: I5adfd4aee30117c7db153b37d26818bef2bd584b

---
+++ aosp_X00P.mk
@@ -0,0 +1,37 @@
+#
+# Copyright (C) 2020 The PixelExperience Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+DEVICE_PATH := device/asus/X00P
+
+# Inherit from source
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o.mk)
+
+# Inherit common Pixel Experience stuff.
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+TARGET_GAPPS_ARCH := arm64
+TARGET_BOOT_ANIMATION_RES := 720
+
+# Inherit device stuff
+$(call inherit-product, $(DEVICE_PATH)/device.mk)
+
+# Device identifiers.
+PRODUCT_DEVICE := X00P
+PRODUCT_NAME := aosp_X00P
+PRODUCT_BRAND := asus
+PRODUCT_MODEL := ASUS_X00PD
+PRODUCT_MANUFACTURER := asus
+TARGET_VENDOR := asus
+BOARD_VENDOR := asus
+
+PRODUCT_GMS_CLIENTID_BASE := android-asus
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="WW_X00PD-user 8.0.0 OPR1.170623.032 15.00.1801.31 release-keys"
+
+# BUILD_FINGERPRINT
+BUILD_FINGERPRINT := asus/WW_X00PD/ASUS_X00P_2:8.0.0/OPR1.170623.032/15.00.1801.31:user/release-keys
