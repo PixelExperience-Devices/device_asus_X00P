58f10facb0143e9e0d84e373b5dc44ea1b9b3a86
iamsaalim <saalimquadri1@gmail.com>
2020-09-07 08:48:57 +0000

    X00P: Cleanup sepolicy * prepare also
    Change-Id: I582317fb1a8a05ffa9787f2dd203777fc0d57580

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -185,7 +185,6 @@ VENDOR_SECURITY_PATCH := 2019-07-05
 # SELinux
 include device/qcom/sepolicy/sepolicy.mk
 BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
-BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
 
 # Treble
 BOARD_PROPERTY_OVERRIDES_SPLIT_ENABLED := true
