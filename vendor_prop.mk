2fbaa04573df033b87235a4e87aac8a4df575e79
iamsaalim <saalimquadri1@gmail.com>
2020-09-13 13:19:31 +0530

    Revert "X00P: Force disable bluetooth audio hal 2.0"
    This reverts commit 5458b3b017616014e74cba96eb1cd7eb30f861cf.

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -52,11 +52,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Bluetooth
 PRODUCT_PROPERTY_OVERRIDES += \
-    bluetooth.hfp.client=1 \
-    vendor.qcom.bluetooth.soc=smd \
-    ro.bluetooth.hfp.ver=1.7 \
-    ro.qualcomm.bt.hci_transport=smd \
-    persist.bluetooth.bluetooth_audio_hal.disabled=true
+    vendor.qcom.bluetooth.soc=smd
 
 # Boot
 PRODUCT_PROPERTY_OVERRIDES += \
