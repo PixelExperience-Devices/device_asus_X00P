1d3ff754a2b1d772b6aa524caedbd9b74db03b82
LuK1337 <priv.luk@gmail.com>
2020-11-20 14:04:07 +0100

    X00P: Disable qti-telephony-common injection
    Change-Id: I2b80a3beb931d11db4b8367cded738e935797002
    Signed-off-by: danascape <saalimquadri1@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -187,7 +187,6 @@ TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/fstab.qcom
 
 # RIL
 ENABLE_VENDOR_RIL_SERVICE := true
-TARGET_PROVIDES_QTI_TELEPHONY_JAR := true
 TARGET_USES_OLD_MNC_FORMAT := true
 
 # Security patch level
